10e36f4c5b5d919dbf47d2b996773f24c76f7b47
FelixEgge <felix.egge@gmail.com>
2023-05-19 22:33:05 +0200

    Update MemberPage.js

--- src/components/MemberPage.js
+++ src/components/MemberPage.js
@@ -60,7 +60,7 @@ class MemberPage extends Component{
                     <aside className="profile-card">
                     <header>
                       <h1>Hej! </h1>
-                      <h2>Swisha 70kr till 072 394 68 07 för att aktivera ditt konto</h2>
+                      <h2>Swisha 70kr till 076 872 35 38 för att aktivera ditt konto</h2>
                     </header>
                     <div className="activate-panel">
                       <input  className="button"  placeholder="Jag har swishat" onClick={this.activate}></input>
