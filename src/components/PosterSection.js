fae7b1522e9360bcec9dc90f396b9231ca7217d2
Kristian Houe Gustafsson <kriho388@student.liu.se>
2023-08-23 18:04:45 +0200

    Uppdaterat alla filmer för HT23

--- src/components/PosterSection.js
+++ src/components/PosterSection.js
@@ -19,13 +19,13 @@ class PosterSection extends Component {
     }
     this.IMAGES = [
       {src: 'https://api.stubinen.org/img/postersHT23/BM.jpg', caption: 'Birdman', date: "2023-09-10",text: translate("BeskrivFilm1"), vidID :"https://www.imdb.com/video/vi1378069529/?playlistId=tt2562232&ref_=ext_shr_lnk"},
-      {src: 'https://api.stubinen.org/img/postersVT23/WPP.jpg', caption: 'The Worst Person in the World', date: "2023-02-05",text: translate("BeskrivFilm2"), vidID :"https://www.imdb.com/video/vi2032321305/?playlistId=tt10370710?ref_=ext_shr_lnk"},
-      {src: 'https://api.stubinen.org/img/postersVT23/ANP.jpg', caption: 'Apocalypse Now', date: "2023-02-19",text: translate("BeskrivFilm3"), vidID :"https://www.imdb.com/video/vi3543773721/?playlistId=tt0078788?ref_=ext_shr_lnk"},
-      {src: 'https://api.stubinen.org/img/postersVT23/LMSP.jpg', caption: 'Little Miss Sunshine', date: "2023-03-05",text: translate("BeskrivFilm4"), vidID :"https://www.imdb.com/video/vi3667263769/?playlistId=tt0449059?ref_=ext_shr_lnk"},
-      {src: 'https://api.stubinen.org/img/postersVT23/JP.jpg', caption: 'The Hunt (Jakten)', date: "2023-04-02",text: translate("BeskrivFilm5"), vidID :"https://www.imdb.com/video/vi2244519449/?playlistId=tt2106476?ref_=ext_shr_lnk"},
-      {src: 'https://api.stubinen.org/img/postersVT23/MV.png', caption: 'Members Choice', date: "2023-04-30",text: translate("BeskrivFilm6"), vidID :"https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"},
-      {src: 'https://api.stubinen.org/img/postersVT23/CKP.jpg', caption: 'Citizen Kane', date: "2023-04-16",text: translate("BeskrivFilm7"), vidID :"https://www.imdb.com/video/vi568630553/?playlistId=tt0033467?ref_=ext_shr_lnk"},
-      {src: 'https://api.stubinen.org/img/postersVT23/ABP.jpg', caption: 'American Beauty', date: "2023-05-14",text: translate("BeskrivFilm8"), vidID :"https://www.imdb.com/video/vi788506137/?playlistId=tt0169547?ref_=ext_shr_lnk"},
+      {src: 'https://api.stubinen.org/img/postersHT23/CB.jpg', caption: 'Casablanca', date: "2023-09-24",text: translate("BeskrivFilm2"), vidID :"https://www.youtube.com/watch?v=MF7JH_54d8c"},
+      {src: 'https://api.stubinen.org/img/postersHT23/SM.jpg', caption: 'Superman (1978)', date: "2023-10-08",text: translate("BeskrivFilm3"), vidID :"https://www.imdb.com/video/vi4168655897/?playlistId=tt0078346&ref_=ext_shr_lnk"},
+      {src: 'https://api.stubinen.org/img/postersHT23/CKE.jpg', caption: 'Chungking Express', date: "2023-10-15",text: translate("BeskrivFilm4"), vidID :"https://www.imdb.com/video/vi594918937/?ref_=ttvi_vi_imdb_1"},
+      {src: 'https://api.stubinen.org/img/postersHT23/SOTL.jpg', caption: 'The Silence of the Lambs', date: "2023-29-10",text: translate("BeskrivFilm5"), vidID :"https://www.imdb.com/video/vi3377380121/?playlistId=tt0102926&ref_=ext_shr_lnk"},
+      {src: 'https://api.stubinen.org/img/postersHT23/MV.png', caption: 'Members Choice', date: "2023-11-26",text: translate("BeskrivFilm6"), vidID :"https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"},
+      {src: 'https://api.stubinen.org/img/postersHT23/BH.jpg', caption: 'Boyhood', date: "2023-12-03",text: translate("BeskrivFilm7"), vidID :"https://www.imdb.com/video/vi1306504217/?playlistId=tt1065073&ref_=ext_shr_lnk"},
+      {src: 'https://api.stubinen.org/img/postersHT23/DS.jpg', caption: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb', date: "2023-12-10",text: translate("BeskrivFilm8"), vidID :"https://www.imdb.com/video/vi3034955545/?ref_=ext_shr_lnk"},
     ];
     this.IMAGES.sort(function(a,b) {
         return new Date(a.date) - new Date(b.date);
