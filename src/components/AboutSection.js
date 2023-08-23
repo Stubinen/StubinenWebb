1f14a9f17d6b77afca8c911852cb47b920918825
Kristian Houe Gustafsson <kriho388@student.liu.se>
2023-08-23 20:54:19 +0200

    Fix med post bilder 2

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -8,12 +8,12 @@ class AboutSection extends Component {
     constructor(props) {
       super(props);
         this.IMAGES = [
-          { src: 'https://api.stubinen.org/img/styrelsen2324/temp.jpg', name: 'Thea',roll: translate("Ordförande")},
-          { src: 'https://api.stubinen.org/img/styrelsen2324/temp.jpg', name: 'Kristian',roll: translate("Kassör")},
-          { src: 'https://api.stubinen.org/img/styrelsen2324/temp.jpg', name: 'Tobias',roll: translate("Filmansvarig")},
-          { src: 'https://api.stubinen.org/img/styrelsen2324/temp.jpg', name: 'Kasper',roll: translate("Marknadsföring")},
-          { src: 'https://api.stubinen.org/img/styrelsen2324/temp.jpg', name: 'Victor',roll: translate("Marknadsföring")},
-          { src: 'https://api.stubinen.org/img/styrelsen2324/temp.jpg', name: 'Emma',roll: translate("Sekreterare")},
+          { src: 'https://api.stubinen.org/img/styrelsen2324/Temp.jpg', name: 'Thea',roll: translate("Ordförande")},
+          { src: 'https://api.stubinen.org/img/styrelsen2324/Temp.jpg', name: 'Kristian',roll: translate("Kassör")},
+          { src: 'https://api.stubinen.org/img/styrelsen2324/Temp.jpg', name: 'Tobias',roll: translate("Filmansvarig")},
+          { src: 'https://api.stubinen.org/img/styrelsen2324/Temp.jpg', name: 'Kasper',roll: translate("Marknadsföring")},
+          { src: 'https://api.stubinen.org/img/styrelsen2324/Temp.jpg', name: 'Victor',roll: translate("Marknadsföring")},
+          { src: 'https://api.stubinen.org/img/styrelsen2324/Temp.jpg', name: 'Emma',roll: translate("Sekreterare")},
       ];
     }
     render() {
