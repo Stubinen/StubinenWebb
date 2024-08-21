ae424f6a01de0324d5db8a2bc0f66442cd9b58e1
eLdOchLagor <94749409+eLdOchLagor@users.noreply.github.com>
2024-08-21 19:58:13 +0200

    Updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,11 @@ import logo from "../img/Logostubinen.png"
 function Header() {
     
     return (
-      <div className="header">
-        <img id="imgLogo" src={logo} alt="Stubinens logo" />
-        <h1 id="titel-h1">  </h1>
-        
-      </div>
+      <div className="header"></div>
     )
 }
   
   export default Header
+
+  // <img id="imgLogo" src={logo} alt="Stubinens logo" />
+  // <h1 id="titel-h1">  </h1>
