7a18020b067c491172bdd5f1ee4030c0d4bb452a
eLdOchLagor <94749409+eLdOchLagor@users.noreply.github.com>
2024-08-28 14:34:24 +0200

    gh-pages build + vite-plugin-react-pages for managing routes correctly

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { Pages } from 'vite-plugin-react-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    outDir: "build",
+  },
+  plugins: [react(), Pages()],
 })
