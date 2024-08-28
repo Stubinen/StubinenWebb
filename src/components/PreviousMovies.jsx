f55531a3c7d58aecdef2bed6a6152af5b726ed28
eLdOchLagor <94749409+eLdOchLagor@users.noreply.github.com>
2024-08-29 00:00:29 +0200

    fix

--- src/components/PreviousMovies.jsx
+++ src/components/PreviousMovies.jsx
@@ -4,16 +4,16 @@ import previousMovies from "../data/previouslyShownMovies.js"
 function PreviousMovies() {
 
     // Sort movies by soonest to latest date
-    previousMovies.sort((a, b) => b.date - a.date);
+    previousMovies.sort((a, b) => a.date - b.date);
 
     // Function to chunk the array into smaller arrays of a specified size
     const chunkArray = (array, chunkSize) => {
-    const chunks = [];
-    for (let i = 0; i < array.length; i += chunkSize) {
-      chunks.push(array.slice(i, i + chunkSize));
-    }
-    return chunks;
-  };
+      const chunks = [];
+      for (let i = 0; i < array.length; i += chunkSize) {
+        chunks.push(array.slice(i, i + chunkSize));
+      }
+      return chunks;
+    };
 
   // Split the movies array into chunks of 8
   const movieChunks = chunkArray(previousMovies, 16);
@@ -31,7 +31,7 @@ function PreviousMovies() {
 
         {movieChunks.map((chunk, index) => (
           <div key={index}>
-            <h1 id="titel-h1">{chunk[15].date.getFullYear().toString().slice(2,4) + "/" + chunk[0].date.getFullYear().toString().slice(2,4)}</h1>
+            <h1 id="titel-h1">{chunk[0].date.getFullYear().toString().slice(2,4) + "/" + chunk[15].date.getFullYear().toString().slice(2,4)}</h1>
             <div className="previousMovies">
               {chunk.map((m, i) => (
                 <img key={i} src={m.image} alt={m.name} />
