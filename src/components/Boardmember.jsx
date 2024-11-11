a1d041b43403077d594df127316c13ff22d76faf
eLdOchLagor <94749409+eLdOchLagor@users.noreply.github.com>
2024-11-11 12:57:16 +0100

    McLovin card test

--- src/components/Boardmember.jsx
+++ src/components/Boardmember.jsx
@@ -2,6 +2,7 @@ import BoardmemberCSS from "./styles/Boardmember.module.css"
 import MemberTable from "./MemberTable"
 import ActivationTable from "./ActivationTable"
 import { useState } from "react";
+import MemberCard from "./MemberCard";
 
 function Boardmember(props) {
     const [showUsers, setShowUsers] = useState(false);
@@ -20,6 +21,7 @@ function Boardmember(props) {
     return (
       <div className={BoardmemberCSS.container}>
         <h1 id={BoardmemberCSS.bigTitle}>Välkommen {props.data.first_name}</h1>
+        <MemberCard />
         <div>
           <button onClick={handleShowUsers}>VISA MEDLEMMAR</button>
           <button onClick={handleShowActivation}>VISA ICKE-AKTIVERADE MEDLEMMAR</button>
---
+++ src/components/MemberCard.jsx
@@ -0,0 +1,14 @@
+import MemberCardCSS from "./styles/MemberCard.module.css"
+import cardImage from "/images/McLovin.png"
+
+function MemberCard() {
+    
+    return (
+      <div id={MemberCardCSS.cardImage}>
+        <h1 id={MemberCardCSS.text}>ddd</h1>
+      </div>
+      
+    )
+}
+  
+  export default MemberCard
