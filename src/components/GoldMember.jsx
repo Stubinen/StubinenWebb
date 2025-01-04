f90fe608c62c41d35455c2ec90bcf8f41d5147cd
eLdOchLagor <94749409+eLdOchLagor@users.noreply.github.com>
2025-01-04 16:11:33 +0100

    Member profile styling

--- src/components/GoldMember.jsx
+++ src/components/GoldMember.jsx
@@ -1,11 +1,13 @@
 import MemberCard from "./MemberCard"
+import GoldMemberCSS from "./styles/GoldMember.module.css"
 
 function GoldMember(props) {
     
     return (
-      <div>
-        <h1>GoldMember</h1>
+      <div className={GoldMemberCSS.container}>
+        <h1 id={GoldMemberCSS.bigTitle}>Välkommen {props.data.first_name}</h1>
         <MemberCard data={props.data}/>
+        <h1 id={GoldMemberCSS.title}>Gold Member</h1>
       </div>
     )
 }
--- src/components/NormalMember.jsx
+++ src/components/NormalMember.jsx
@@ -1,11 +1,13 @@
 import MemberCard from "./MemberCard"
+import NormalMemberCSS from "./styles/NormalMember.module.css"
 
 function NormalMember(props) {
     
     return (
       <div>
-        <h1>Normal</h1>
+        <h1 id={NormalMemberCSS.bigTitle}>Välkommen {props.data.first_name}</h1>
         <MemberCard data={props.data}/>
+        <h1 id={NormalMemberCSS.title}>Normal member</h1>
       </div>
     )
 }
